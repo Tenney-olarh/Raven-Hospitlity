287e2a9937bc6f0f921d0540a6aa11f0594128b1
Prosper Chukwuemelie <111505389+pchukwuemelie@users.noreply.github.com>
2026-03-29 09:56:51 +0100

    Update CSS import path in otp.jsx

--- src/Pages/Auth/otp.jsx
+++ src/Pages/Auth/otp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import "./Otp.css";
+import "./styles/home/otp.css";
 
 export default function PasscodeAuthPage() {
   const [passcode, setPasscode] = useState(Array(6).fill(""));
